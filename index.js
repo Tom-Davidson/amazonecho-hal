d5b30776be8f96e73db2d68d2f8c7d4510131283
Tom Davidson <tom@davidson.me.uk>
2016-06-08 11:08:37 +0100

    default to port 80 when not in env

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config({silent: true});
 const server = new Hapi.Server();
 server.connection({
   host: '0.0.0.0',
-  port: parseInt(process.env.PORT, 10)
+  port: parseInt(process.env.PORT, 10) || 80
 });
 server.route({
   method: 'GET',
