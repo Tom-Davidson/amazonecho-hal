b044648f44083a05b8e5cc7848e8a767965655c8
Tom Davidson <tom@davidson.me.uk>
2016-06-08 16:30:16 +0100

    crudely parses the request and changes the response message

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Hapi = require('hapi');
 const constants = require('./constants');
 require('dotenv').config({silent: true});
 
+console.log(process.env.PORT);
+
 const server = new Hapi.Server();
 server.connection({
   host: '0.0.0.0',
@@ -11,21 +13,44 @@ server.connection({
 });
 server.route({
   method: 'GET',
-  path:'/ping',
+  path: '/ping',
   handler: function (request, reply) {
     return reply({"ping":"pong"});
   }
 });
 server.route({
   method: 'POST',
-  path:'/hal',
+  path: '/hal',
   handler: function (request, reply) {
+    const alexaRequest = JSON.parse(request.payload);
+    let message = 'Unable to parse request';
+    if(
+      alexaRequest
+      && typeof alexaRequest.request !== "undefined"
+      && typeof alexaRequest.request.type !== "undefined"
+    ){
+      switch(alexaRequest.request.type){
+        case 'IntentRequest':
+            switch(alexaRequest.request.intent.name){
+              case 'OpenAirlock':
+                  message = 'I\'m sorry, Dave. I\'m afraid I can\'t do that.';
+                break;
+              default:
+                  message = 'Unrecognised IntentRequest received';
+                break;
+            }
+          break;
+        default:
+            message = 'Unknown request type';
+          break;
+      }
+    }
     return reply({
       'version': '1.0',
       'response': {
         'outputSpeech': {
           'type': 'PlainText',
-          'text': 'Request sucessfully received'
+          'text': message
         },
         'card': {
           'type': 'Simple'
