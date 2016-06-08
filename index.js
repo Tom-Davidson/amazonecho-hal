e35cdb37a835edc26e0a6481244429050a041fd5
Tom Davidson <tom@davidson.me.uk>
2016-06-08 11:06:52 +0100

    basic HAPI server

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Hapi = require('hapi');
+require('dotenv').config({silent: true});
+
+const server = new Hapi.Server();
+server.connection({
+  host: '0.0.0.0',
+  port: parseInt(process.env.PORT, 10)
+});
+server.route({
+  method: 'GET',
+  path:'/ping',
+  handler: function (request, reply) {
+    return reply('{"ping":"pong"}');
+  }
+});
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+});
+
+module.exports = server;
---
+++ test/test.js
@@ -0,0 +1,30 @@
+var test   = require('tape');
+var server = require("../index.js");
+
+test("Basic HTTP Tests - GET /", function(t) { // t
+  server.inject(
+    {
+      method: "GET",
+      url: "/"
+    },
+    function(response) {
+      t.equal(response.statusCode, 404);
+      t.equal(response.payload, '{"statusCode":404,"error":"Not Found"}');
+      server.stop(t.end);
+    }
+  );
+});
+test("Basic HTTP Tests - GET /ping", function(t) { // t
+  server.inject(
+    {
+      method: "GET",
+      url: "/ping"
+    },
+    function(response) {
+      t.equal(response.statusCode, 200);
+      t.equal(response.payload, '{"ping":"pong"}');
+      t.equal(response.payload.length, 15);
+      server.stop(t.end);
+    }
+  );
+});
